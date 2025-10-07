9cc73373f2cff8fdf3b0d46a285e808f1b1e0ab9
quynh150105 <quynh1512005@gmail.com>
2025-10-07 22:05:41 +0700

    variable in golang

---
+++ Bai2/app.go
@@ -0,0 +1,35 @@
+package main
+var address string = "Hanoi"
+
+func main() {
+
+	/* Khai bao bien voi var 
+	Co the ko khai bao kieu du lieu hoac gia tri
+	// Cach khai bao bien : var ten_bien Kieu_du_lieu = ""
+	// Co the ko can khai bao kieu du lieu, go tu hieu
+
+	var fullName  = "Hello"
+
+	// Khai bao ko gan gia tr: var ten_bien Kieu_du_lieu
+	// Khong gan hay khai bao => default value = 0
+
+	var age int
+	age = 10
+	fmt.Println(age) => 10
+	fmt.Print(fullName) => "Hello"
+	*/
+
+	/* Khai bao bien dung ":="
+	Kieu du lieu cua bien duoc suy ra tu gia tri (Trinh bien dich quy dinh)
+	Luon phai khai bao gia tri
+
+	variableName := value
+	phone  := "12345678"
+	fmt.Println(phone) // => 12345678
+	fmt.Println(address)
+	*/
+
+
+
+	
+}
